4b33a23d87f9cde5bbb64d8fb88b8c3dfc61ceae
Rodrigo <roliveira@webeleven.com.br>
2017-07-22 16:29:33 -0300

    Responsive images

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Lead;
+use App\Models\Lead;
 use Illuminate\Http\Request;
 
 class LeadController extends Controller
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,36 +13,6 @@
     
     <!-- Styles -->
     <style>
-        
-        
-        div {
-            padding: 0px;
-            margin: 0px;
-        }
-        
-        .uno-1 {
-            background: url('/images/uno_01.jpg') no-repeat;
-            width: 100%;
-            height: 770px;
-        }
-        
-        .uno-2 {
-            background: url('/images/uno_02.png') no-repeat;
-            width: 100%;
-            height: 770px;
-        }
-        
-        .uno-3 {
-            background: url('/images/uno_03.png') no-repeat;
-            width: 100%;
-            height: 1000px;
-        }
-        
-        .uno-4 {
-            width: 100%;
-            height: 100px;
-        }
-        
         .uno-5 {
             color: #fff;
             background: #ff2d7f;
@@ -58,42 +28,49 @@
     </style>
 </head>
 <body>
-    <div class="uno-1"></div>
-    <div class="uno-2"></div>
-    <div class="uno-3"></div>
-    <div class="uno-4"></div>
-    <div class="uno-5">
-        <div class="center">
-            <h1 clas="center">FAÇA SUA INSCRIÇÃO</h1>
+    <div class="row">
+        <div class="col-xs-12">
+            <img src="{{ asset('/images/uno_01.jpg') }}" class="img-responsive" alt="">
         </div>
-        <div class="col-xs-12 col-md-6 col-md-offset-3">
-            <form action="{{ route('register') }}" method="POST">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="name">Nome: </label>
-                    <input type="text" class="form-control" name="name">
-                </div>
-                <div class="form-group">
-                    <label for="name">E-mail: </label>
-                    <input type="text" class="form-control" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="name">Profissão: </label>
-                    <input type="text" class="form-control" name="occupation">
-                </div>
-                <div class="form-group">
-                    <label for="name">Área de interesse: </label>
-                    <select name="areaOfInterest" id="areaOfInterest" class="form-control">
-                        <option value="developer">Desenvolvedor</option>
-                        <option value="design">Design</option>
-                        <option value="sales">Vendas</option>
-                        <option value="marketin">Marketing</option>
-                    </select>
-                </div>
-                <div class="center">
-                    <button type="submit" class="btn btn-success" style="background: #7ac043;">Enviar</button>
-                </div>
-            </form>
+        <div class="col-xs-12">
+            <img src="{{ asset('/images/uno_02.png') }}" class="img-responsive" alt="">
+        </div>
+        <div class="col-xs-12">
+            <img src="{{ asset('/images/uno_03.png') }}" class="img-responsive" alt="">
+        </div>
+        <div class="col-xs-12 uno-5">
+            <div class="center">
+                <h1 clas="center">FAÇA SUA INSCRIÇÃO</h1>
+            </div>
+            <div class="col-xs-12 col-md-6 col-md-offset-3">
+                <form action="{{ route('register') }}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name">Nome: </label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">E-mail: </label>
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Profissão: </label>
+                        <input type="text" class="form-control" name="occupation">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Área de interesse: </label>
+                        <select name="areaOfInterest" id="areaOfInterest" class="form-control">
+                            <option value="developer">Desenvolvedor</option>
+                            <option value="design">Design</option>
+                            <option value="sales">Vendas</option>
+                            <option value="marketin">Marketing</option>
+                        </select>
+                    </div>
+                    <div class="center">
+                        <button type="submit" class="btn btn-success" style="background: #7ac043;">Enviar</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
